944300ee00954ffff9754cec1c69eb78c0729d24
Andor Vári-Kakas <varikakasandor@gmail.com>
2026-08-18 17:24:28 +0200

    File structure made less restrictive

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const profile = {
+    name: "Andor Vári-Kakas",
+    role: "PhD student in Theoretical Computer Science",
+    affiliation: "ETH Zürich",
+  
+    email: "avarikakas (at) inf (dot) ethz (dot) ch",
+    linkedin: "https://www.linkedin.com/in/andor-vari-kakas/",
+    scholar: "https://scholar.google.com/citations?user=-DkMoPUAAAAJ",
+  
+    photo: "/profile.webp",
+    cv: "/cv.pdf",
+  };
